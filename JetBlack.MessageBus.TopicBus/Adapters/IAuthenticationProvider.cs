a09f0650c5a21b3266f26acaa2dfd08581f3ff92
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-06-12 12:17:45 +0100

    Split out nested class.

---
+++ JetBlack.MessageBus.TopicBus/Adapters/IAuthenticationProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using JetBlack.MessageBus.TopicBus.Messages;
+
+namespace JetBlack.MessageBus.TopicBus.Adapters
+{
+    public interface IAuthenticationProvider
+    {
+        void RequestAuthentication(ForwardedAuthenticationRequest forwardedAuthenticationRequest);
+        IObservable<AuthenticationResponse> ToResponseObservable(int clientId);
+    }
+}
