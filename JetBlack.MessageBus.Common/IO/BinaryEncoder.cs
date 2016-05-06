9b9e97cd67921ff565dbfe3b5c74fe434159ad99
rob-blackbourn <rob.blackbourn@googlemail.com>
2016-05-06 07:56:51 +0100

    Added a default encoder.

---
+++ JetBlack.MessageBus.Common/IO/BinaryEncoder.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace JetBlack.MessageBus.Common.IO
+{
+    public class BinaryEncoder<T> : IByteEncoder<T>
+    {
+        static readonly BinaryFormatter BinaryFormatter = new BinaryFormatter();
+
+        public T Decode(byte[] bytes)
+        {
+            if (bytes == null || bytes.Length <= 0)
+                return default(T);
+
+            using (var stream = new MemoryStream(bytes))
+            {
+                return (T)BinaryFormatter.Deserialize(stream);
+            }
+        }
+
+        public byte[] Encode(T obj)
+        {
+            using (var stream = new MemoryStream())
+            {
+                BinaryFormatter.Serialize(stream, obj);
+                stream.Flush();
+                return stream.GetBuffer();
+            }
+        }
+    }
+}
