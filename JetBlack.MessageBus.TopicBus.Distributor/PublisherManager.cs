f1e9336e2a56213c97328c82fb1acf81a43f122e
rob-blackbourn <rob.blackbourn@googlemail.com>
2016-05-10 08:21:58 +0100

    Split out the publisher data structure into a repository.

--- JetBlack.MessageBus.TopicBus.Distributor/PublisherManager.cs
+++ JetBlack.MessageBus.TopicBus.Distributor/PublisherManager.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reactive.Subjects;
-using JetBlack.MessageBus.Common.Collections;
-using JetBlack.MessageBus.TopicBus.Messages;
 using log4net;
+using JetBlack.MessageBus.TopicBus.Messages;
 
 namespace JetBlack.MessageBus.TopicBus.Distributor
 {
@@ -11,7 +10,7 @@ namespace JetBlack.MessageBus.TopicBus.Distributor
     {
         private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        private readonly TwoWaySet<string, Interactor> _topicsAndPublishers = new TwoWaySet<string, Interactor>();
+        private readonly PublisherRepository _repository = new PublisherRepository();
         private readonly ISubject<SourceMessage<IEnumerable<string>>> _stalePublishers = new Subject<SourceMessage<IEnumerable<string>>>();
 
         public IObservable<SourceMessage<IEnumerable<string>>> StalePublishers
@@ -21,19 +20,19 @@ namespace JetBlack.MessageBus.TopicBus.Distributor
 
         public void SendMulticastData(Interactor publisher, MulticastData multicastData, Interactor subscriber)
         {
-            _topicsAndPublishers.Add(publisher, multicastData.Topic);
+            _repository.AddPublisher(publisher, multicastData.Topic);
             subscriber.SendMessage(multicastData);
         }
 
         public void SendUnicastData(Interactor publisher, UnicastData unicastData, Interactor subscriber)
         {
-            _topicsAndPublishers.Add(publisher, unicastData.Topic);
+            _repository.AddPublisher(publisher, unicastData.Topic);
             subscriber.SendMessage(unicastData);
         }
 
         public void OnClosedInteractor(Interactor interactor)
         {
-            var topicsWithoutPublishers = _topicsAndPublishers.Remove(interactor);
+            var topicsWithoutPublishers = _repository.RemovePublisher(interactor);
             if (topicsWithoutPublishers != null)
                 _stalePublishers.OnNext(SourceMessage.Create(interactor, topicsWithoutPublishers));
         }
---
+++ JetBlack.MessageBus.TopicBus.Distributor/PublisherRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using JetBlack.MessageBus.Common.Collections;
+
+namespace JetBlack.MessageBus.TopicBus.Distributor
+{
+    internal class PublisherRepository
+    {
+        private readonly TwoWaySet<string, Interactor> _topicsAndPublishers = new TwoWaySet<string, Interactor>();
+
+        public void AddPublisher(Interactor publisher, string topic)
+        {
+            _topicsAndPublishers.Add(publisher, topic);
+        }
+
+        public IEnumerable<string> RemovePublisher(Interactor publisher)
+        {
+            return _topicsAndPublishers.Remove(publisher);
+        }
+    }
+}
