6d23b8b2b59c2d90bdbb7b675a31868c5f5b69c0
rob-blackbourn <rob.blackbourn@googlemail.com>
2016-05-13 14:36:45 +0100

    Added a test for two way sets.

---
+++ JetBlack.MessageBus.Common.Test/Collections/TwoWaySetTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JetBlack.MessageBus.Common.Collections;
+
+namespace JetBlack.MessageBus.Common.Test.Collections
+{
+    [TestClass]
+    public class TwoWaySetTest
+    {
+        [TestMethod]
+        public void SmokeTest()
+        {
+            var twoWaySet = new TwoWaySet<string, int>();
+
+            const string a = "a", b = "b", c = "c";
+            const int one = 1, two = 2, three = 3;
+
+            twoWaySet.Add(a, one);
+            twoWaySet.Add(two, b);
+            twoWaySet.Add(b, three);
+            Assert.IsTrue(twoWaySet.ContainsKey(a));
+            Assert.IsTrue(twoWaySet.ContainsKey(one));
+            Assert.IsTrue(twoWaySet.ContainsKey(b));
+            Assert.IsTrue(twoWaySet.ContainsKey(two));
+            Assert.IsTrue(twoWaySet.ContainsKey(three));
+            Assert.IsFalse(twoWaySet.ContainsKey(c));
+
+            ISet<string> words;
+            Assert.IsTrue(twoWaySet.TryGetValue(one, out words));
+            Assert.AreEqual(1, words.Count);
+            ISet<int> numbers;
+            Assert.IsTrue(twoWaySet.TryGetValue(b, out numbers));
+            Assert.AreEqual(2, numbers.Count);
+            Assert.IsFalse(twoWaySet.TryGetValue(c, out numbers));
+        }
+    }
+}
