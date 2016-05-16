c61d4aa7367bf92e15d5cfdb9c130cb7363a435e
rob-blackbourn <rob.blackbourn@googlemail.com>
2016-05-16 17:05:43 +0100

    Added a feed bus distributor test harness.

---
+++ JetBlack.MessageBus.Examples.FeedBusConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.ServiceModel.Channels;
+using JetBlack.MessageBus.FeedBus.Distributor;
+using JetBlack.MessageBus.FeedBus.Distributor.Config;
+
+namespace JetBlack.MessageBus.Examples.FeedBusConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            var configuration = (ConfigurationSectionHandler)ConfigurationManager.GetSection("authFeedBusDistributor");
+
+            const int maxBufferPoolSize = 100;
+            const int maxBufferSize = 100000;
+            var ipAddress = IPAddress.Any;
+            const int port = 9090;
+
+            var bufferManager = BufferManager.CreateBufferManager(maxBufferPoolSize, maxBufferSize);
+            var endpoint = new IPEndPoint(ipAddress, port);
+
+            var server = new Server(endpoint, bufferManager, configuration.DefaultConfig);
+
+            Console.WriteLine("Press <ENTER> to quit");
+            Console.ReadLine();
+
+            server.Dispose();
+        }
+    }
+}
